fce1baf90742fadaae31ef46470da312126c750a
chipmunk-dev <devquokkajeong@gmail.com>
2023-06-23 16:28:32 +0900

    feat: 로그인 벨리데이션

--- src/router/auth.route.ts
+++ src/router/auth.route.ts
@@ -1,8 +1,33 @@
 import * as express from 'express';
+import { body, validationResult } from 'express-validator';
 import { login } from '../controller/member.controller';
 
 const route = express.Router();
 
-route.post('/login', login);
+route.post(
+  '/login',
+  [
+    body('id')
+      .notEmpty()
+      .withMessage('아이디가 비어있습니다.')
+      .isEmail()
+      .withMessage('아이디는 이메일 형식이여야 합니다.'),
+    body('password').notEmpty().withMessage('패스워드가 비어있습니다.'),
+    (
+      request: express.Request,
+      response: express.Response,
+      next: express.NextFunction,
+    ) => {
+      const result = validationResult(request);
+
+      if (result.isEmpty()) {
+        return next();
+      }
+
+      return response.status(400).json({ errors: result.array() });
+    },
+  ],
+  login,
+);
 
 export default route;
