c9fa007e7ed3a06ab058bbe2fa4a48fd5c909327
chipmunk-dev <devquokkajeong@gmail.com>
2023-06-23 10:28:20 +0900

    feat: question url path 변경 적용

--- src/router/question.route.ts
+++ src/router/question.route.ts
@@ -10,10 +10,10 @@ import { auth } from '../middleware/auth';
 
 const route = express.Router();
 
-route.post('/', auth, createQuestion);
+route.post('/ask', auth, createQuestion);
 route.get('/', getQuestionList);
 route.get('/:questionId', getQuestionOne);
-route.patch('/:questionId', auth, updateQuestion);
+route.patch('/edit/:questionId', auth, updateQuestion);
 route.delete('/:questionId', auth, deleteQuestion);
 
 export default route;
